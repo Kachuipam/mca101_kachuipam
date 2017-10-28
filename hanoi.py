7ea2b6ed78bf0b37bb3e7c84f1622f4e03459786
Chirag Bohet <bohet.chirag@gmail.com>
2017-10-28 23:15:34 +0530

    Added Missing code and proper Documentation.
    Changes made - 
    1) Added case if Number of disks entered is 0
    2) Added proper documentation in the function towerOfHanoi() and main().
    3) In main() the variables source,spare and target were not declared or initialized. So I am taking them as user input.  
    4) Other minor changes

--- hanoi.py
+++ hanoi.py
@@ -3,11 +3,16 @@ def towerOfHanoi(n,source,spare,target):
     objective: To Solve the problem of tower of hanoi.
     input parameters:
         n: Number of Disk
-        source: The source 
+        source: The source
+        spare : The spare pole
+        target : The target pole
+    Output : The required disk movement.
     '''
+    
     if n == 1:
         print('Move disk from ',source,'to',target)
-
+    elif n==0:
+        return
     else:
         towerOfHanoi(n-1,source,target,spare)
         print('Move disk from ',source,'to',target)
@@ -15,8 +20,20 @@ def towerOfHanoi(n,source,spare,target):
 
 def main():
     '''
+    Objective : To generate the movements required to solve Tower of Hanoi
+    User Inputs : 
+        n: Number of Disk
+        source: The source
+        spare : The spare pole
+        target : The target pole
+    Output : The required disk movement.
     '''
-    n = int(input('Enter the number of disk: '))
+
+    n = int(input('Enter the number of Disks: '))
+    source = int(input('Enter the Source pole: '))
+    spare = int(input('Enter Spare pole: '))
+    target = int(input('Enter the Target pole: '))
+
     towerOfHanoi(n,source,spare,target)
 
 if __name__=='__main__':
