d415db23e2ed2d3ed4a0a53a9416da74d15c46a1
mca101_kachuipam <31645361+Kachuipam@users.noreply.github.com>
2017-11-03 11:35:54 +0530

    Program to merge two sorted list

---
+++ mergeSorted.py
@@ -0,0 +1,35 @@
+#import pdb
+
+def mergeList(list1,list2,list3,count1 = 0,count2 = 0):
+    #pdb.set_trace()
+    '''
+    Objective: To merge two sorted list
+    '''
+    if (count1 == len(list1) and count2 == len(list2) ):
+        return list3
+        
+    if (count1 == len(list1)):
+        list3.append(list2[count2])
+        count2 = count2+1
+        return mergeList(list1,list2,list3,count1,count2)
+    
+    if (count2 == len(list2)):
+        list3.append(list1[count1])
+        count1 = count1+1
+        return mergeList(list1,list2,list3,count1,count2)   
+    
+    if list1[count1] < list2[count2]:
+        list3.append(list1[count1])
+        count1=count1+1  
+    else:
+        list3.append(list2[count2])
+        count2 = count2+1
+        
+    return mergeList(list1,list2,list3,count1,count2)
+        
+            
+list1 = [1,5,6]
+list2 = [3,9]
+list3 = []
+list3 = mergeList(list1,list2,list3)
+print(list3)
